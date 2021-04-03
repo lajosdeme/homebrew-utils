07f46077e0495c5e6961c59c84fad954c1f88c22
lajosdeme <44027725+lajosdeme@users.noreply.github.com>
2021-04-03 15:12:11 +0200

    Update xcclear.rb

--- xcclear.rb
+++ xcclear.rb
@@ -2,7 +2,7 @@ class Xcclear < Formula
     desc "Command line utility to clear unwanted Xcode files"
     homepage "https://github.com/lajosdeme/xcclear"
     url "https://github.com/lajosdeme/xcclear/releases/download/v1.0.1/xcclear-1.0.1.tar.gz"
-    sha256 "406ee201078c147ca273d204afeb599a4f0a6b1a66318f6fc9b30bca6902410a"
+    sha256 "f1e4f21a570e9d0ae1fb23fe179be07eb23f854823403a128c60c5923c34b103"
 
     bottle :unneeded
 
