686f11b7d6bf1a7741d26cf42cc003e5a2425432
lajosdeme <44027725+lajosdeme@users.noreply.github.com>
2022-12-16 23:40:29 +0100

    remove bottle: unneeded

--- xcclear.rb
+++ xcclear.rb
@@ -4,8 +4,6 @@ class Xcclear < Formula
     url "https://github.com/lajosdeme/xcclear/releases/download/v1.0.1/xcclear-1.0.1.tar.gz"
     sha256 "406ee201078c147ca273d204afeb599a4f0a6b1a66318f6fc9b30bca6902410a"
 
-    bottle :unneeded
-
     def install
         bin.install "xcclear"
     end
